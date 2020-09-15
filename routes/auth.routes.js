4c34350f65222c3372dde6cef8f17ad285756793
Roman <romaniv788@gmail.com>
2020-09-15 09:59:11 +0300

    added server side auth

---
+++ routes/auth.routes.js
@@ -0,0 +1,90 @@
+const {Router} = require('express');
+const  bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator');
+const User = require('../models/User');
+const router = Router();
+
+router.post(
+  '/register',
+  [
+    check('email', 'Некоректний email').isEmail(),
+    check('password', 'Мінімальна довжина паролю 6 символів')
+      .isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некоректні данні при кастомізації'
+        });
+      }
+      const {email, password} = req.body;
+
+      const candidate = await User.findOne({email});
+
+      if (candidate) {
+        return res.status(400).json({message: 'Такий користувач уже створений'});
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({email, password: hashedPassword});
+      await user.save();
+
+      res.status(201).json({message: 'Користувач створений!'});
+
+    } catch (e) {
+      res.status(500).json({message: 'Щось пішло не так =('});
+    }
+  }
+);
+
+router.post(
+  '/login',
+  [
+    check('email', 'Введіть корректний email').normalizeEmail().isEmail(),
+    check('password', 'Введіть пароль').exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некоректні данні при вході в систему.'
+        });
+      }
+
+      const {email, password} = req.body;
+
+      const  user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({message: 'Користувач не знайдений =('});
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({message: 'Неправильний пароль, попробуйте знову.'});
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+        );
+      res.json({token, userId: user.id});
+
+    } catch (e) {
+      res.status(500).json({message: 'Щось пішло не так =('});
+    }
+  }
+);
+
+module.exports = router;
