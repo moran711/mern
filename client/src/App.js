ff957329e425da7e09b7135f1512efaff159846f
Roman <romaniv788@gmail.com>
2020-09-17 09:24:48 +0300

    finished front-end and back-end part of app

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useRoutes} from "./routes";
+import {useAuth} from "./hooks/auth.hook";
+import {AuthContext} from "./context/AuthContext";
+import {Navbar} from "./components/Navbar";
+import {Loader} from "./components/Loader";
+import 'materialize-css';
+
+
+function App() {
+  const {token, login, logout, userId, ready} = useAuth();
+  const isAuthentificated = !!token;
+  const routes = useRoutes(isAuthentificated);
+
+  if (!ready) {
+    return <Loader />
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      token,
+      login,
+      logout,
+      userId,
+      isAuthentificated
+    }}>
+        <Router>
+          { isAuthentificated &&  <Navbar />}
+          <div className="container">
+          {routes}
+          </div>
+        </Router>
+    </AuthContext.Provider>
+  );
+};
+
+export default App;
